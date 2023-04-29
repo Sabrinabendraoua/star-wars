c8f449a4215a6e28c1eac966929ed9df4af89a2c
sabrina bendraoua <sabbendra@gmail.com>
2023-04-29 17:35:59 +0200

    movieList

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 import data from "./assets/data.json";
-console.log(data); // j'ai ma data
+// console.log(data); // j'ai ma data
 
 const App = () => {
   const [movieData, setMovieData] = useState(false);
-
-  const handleClick = () => {
-    setMovieData(true);
-  };
 
   return (
     <main className="background-img">
       <section>
         {data.map((elemMovies) => {
           return (
-            <>
-              <button
-                key={elemMovies.id}
-                onClick={() => {
-                  console.log(elemMovies.name);
-                }}
-              >
-                {elemMovies.name}
-              </button>
-              <div>
-                {elemMovies.actors.map((movies) => {
-                  return (
-                    <>
+            <div>
+              {movieData === false ? (
+                <button
+                  key={elemMovies.id}
+                  onClick={() => {
+                    setMovieData();
+                    // console.log(elemMovies.name);
+                  }}
+                >
+                  {elemMovies.name}
+                </button>
+              ) : (
+                <div>
+                  {elemMovies.actors.map((movies, index) => {
+                    console.log(movies.name);
+                    return (
                       <div>
-                        <img src={movies.picture} />
                         <h3>{movies.name}</h3>
+                        <img src={movies.picture} />
                       </div>
-                    </>
-                  );
-                })}
-              </div>
-            </>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
           );
         })}
       </section>
